7a390ae3dd75902f35b6785e85e917991c307121
Nathan Wu <nathanwu@Nathans-MacBook-Pro.local>
2025-04-06 19:10:14 +0800

    recover

--- _plugins/sort_by_date.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-  module SortByDateFilter
-    def sort_by_date(input)
-      input.sort_by { |item| Date.parse(item.data["due_date"]) }
-    end
-  end
-end
-Liquid::Template.register_filter(Jekyll::SortByDateFilter)
